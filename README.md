79d8f4f0a623be197b2c617bb449055a40d8d834
Abhi Agarwal <me@abhiagarwal.com>
2015-04-06 14:10:50 -0400

    README

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # nget
-Node alternative of go get
+
+Node alternative of go get. Very basic version. The setup process isn't great as I spent 10 minutes making this script. Make the NgetPath directory and export it as a environment variable.
+
+`export NgetPATH="/Users/<yourusername>/node/"`
+
+Download this project and set up:
+
+`export nget="node <path to project>/index.js"
+
+Then just run:
+
+`nget get github.com/abhiagarwal/nget`
+
+To make your life easier at this point you can switch your `nget` to:
+
+`export nget="node $NgetPATH/src/github.com/abhiagarwal/index.js"
+
+Then you're able to use it!
--- index.js
+++ index.js
@@ -2,47 +2,51 @@ var program = require('commander');
 var Git = require("nodegit");
 var fs = require('fs');
 var mkdirp = require('mkdirp');
-var Q = require('q');
+var sys = require('sys')
+var exec = require('child_process').exec;
+
+function puts(error, stdout, stderr) {
+  if(error){
+    console.log(error);
+  }
+}
 
 program
-    .version('0.0.1')
-    .usage('nget')
-    .parse(process.argv);
+  .version('0.0.1')
+  .usage('nget')
+  .parse(process.argv);
 
 if (!process.env.NgetPATH) {
-    console.log("Please set your NgetPATH!");
-    return
+  console.log("Please set your NgetPATH!");
+  return
 }
 
 if (!program.args.length) {
-    program.help();
-    return
+  program.help();
+  return
 } else {
-    if (program.args[0] == "get") {
-        if (program.args.length == 1) {
-            console.log("Please specify repository");
-            return
-        }
-        var splitArray = program.args[1].split("/");
-        var NgetPath =
-            process.env.NgetPATH + "src/" + splitArray[0].toLowerCase() + "/" + splitArray[1].toLowerCase() + "/" + splitArray[2].toLowerCase();
-        var GithubPath =
-            "https://" + splitArray[0].toLowerCase() + "/" + splitArray[1].toLowerCase();
-        try {
-            if (fs.existsSync(NgetPath)) {
-                console.log("Directory already exists!");
-                return
-            }
-        } catch (e) {
-            console.log(e);
-            return
-        }
-        mkdirp(NgetPath);
-        Git.Clone(GithubPath, NgetPath, {ignoreCertErrors: 1})
-          .then(function(repository) {
-            console.log(repository);
-          });
-    } else {
-        program.help();
+  if (program.args[0] == "get") {
+    if (program.args.length == 1) {
+      console.log("Please specify repository");
+      return
     }
+    var splitArray = program.args[1].split("/");
+    var NgetPath =
+        process.env.NgetPATH + "src/" + splitArray[0].toLowerCase() + "/" + splitArray[1].toLowerCase() + "/";
+    var GithubPath =
+        "https://" + splitArray[0].toLowerCase() + "/" + splitArray[1].toLowerCase() + "/" + splitArray[2].toLowerCase() + ".git";
+    console.log(GithubPath);
+    try {
+        if (fs.existsSync(NgetPath + splitArray[2].toLowerCase())) {
+            console.log("Directory already exists!");
+            return
+        }
+    } catch (e) {
+        console.log(e);
+        return
+    }
+    exec("cd " + NgetPath + " && git clone " + GithubPath + " " + splitArray[2].toLowerCase(), puts);
+  } else {
+    program.help();
+  }
 }
